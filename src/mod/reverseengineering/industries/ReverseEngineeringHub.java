b1bb40c525c7be8f8ec83285561d5f06d29f9e27
Darkborderman <reastw1234@gmail.com>
2023-10-26 00:34:26 +0800

    Update

--- src/mod/reverseengineering/industries/ReverseEngineeringHub.java
+++ src/mod/reverseengineering/industries/ReverseEngineeringHub.java
@@ -1,12 +1,10 @@
 package mod.reverseengineering.industries;
 
-import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.impl.campaign.econ.impl.BaseIndustry;
 
 public class ReverseEngineeringHub extends BaseIndustry{
 
     public void apply(){
         super.apply(true);
-        Global.getSector().getMemory().set("REVERSE_ENABLED", true);
     }
 }
